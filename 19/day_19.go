601464ff8d14a79e323c34ce1433924393bf5956
Tim Benjamin <tim@tienjin.com>
2020-12-21 19:29:01 +0000

    another crack at day 19 part 2 (no good)

--- 19/day_19.go
+++ 19/day_19.go
@@ -17,6 +17,9 @@ var test_rules_file = "./test_rules_2.txt"
 var rules_file = test_rules_file
 var input_file = test_input_file
 
+var rule_a string
+var rule_b string
+
 var rules = map[string]string{}
 var input []string
 
@@ -33,6 +36,11 @@ func get_rules() {
 		line = scanner.Text()
 		line_split := strings.Split(line, ": ")
 		rules[line_split[0]] = line_split[1]
+		if line_split[1] == "\"a\"" {
+			rule_a = line_split[0]
+		} else if line_split[1] == "\"b\"" {
+			rule_b = line_split[0]
+		}
 	}
 }
 
@@ -56,7 +64,8 @@ var path = []string{}
 // recursively handle rules with data, ultimately returning true/false if data is length 0?
 func handle_rule(rule_index string, data string) (new_data string, result bool) {
 	path = append(path, rule_index)
-	fmt.Printf("==> handle_rule with index %v [%v] and data %v\n", rule_index, rules[rule_index], data)
+	fmt.Println(rule_index)
+	//fmt.Printf("handle_rule with index %v [%v]\n", rule_index, rules[rule_index])
 	// is it a numeric one or a letter one?
 	rule := rules[rule_index]
 	matched, _ := regexp.MatchString(`\d`, rule)
@@ -65,15 +74,17 @@ func handle_rule(rule_index string, data string) (new_data string, result bool) 
 		//fmt.Println("handle numeric rule:", rule)
 		options := strings.Split(rule, "|")
 		// there might be only 1 option, or 2
+		// save the data in case one option fails and we have to try the other
 		data_original := data
 	outer:
 		for option_index, option := range options {
 			option = strings.TrimSpace(option)
-			fmt.Printf("try option %v which is >>%v<<\n", option_index, option)
 			rule_sequence := strings.Split(option, " ")
+			fmt.Printf(" try option %v which is %v\n", option_index, rule_sequence)
 			for i, r := range rule_sequence {
-				fmt.Println("We have to apply rule:", r)
-				data, result = handle_rule(r, data)
+				//fmt.Println("Apply rule:", r)
+				data, result = handle_rule(r, data) // this disappears down a chain of rules and comes back when it hits an a/b rule
+				//fmt.Printf("-> result: %v - data: %v\n", result, data)
 				if result {
 					// if there are more rules to come, and we have no data left, this should be a dead-end path.
 					if i < len(rule_sequence)-1 && len(data) == 0 {
@@ -111,7 +122,7 @@ func handle_rule(rule_index string, data string) (new_data string, result bool) 
 			} else {
 				data = ""
 			}
-			fmt.Println("Removed a, data is now:", data)
+			fmt.Println("   removed a, data is now:", data)
 			return data, true
 		} else {
 			fmt.Printf("Rule %v fails to match 'a' with data %v\n", rule, data)
@@ -128,7 +139,7 @@ func handle_rule(rule_index string, data string) (new_data string, result bool) 
 			} else {
 				data = ""
 			}
-			fmt.Println("Removed b, data is now:", data)
+			fmt.Println("   removed b, data is now:", data)
 			return data, true
 		} else {
 			fmt.Printf("Rule %v fails to match 'b' with data %v\n", rule, data)
@@ -165,9 +176,93 @@ func run() int {
 	return len(matching_inputs)
 }
 
+func chop(chain string, letter byte) (new_chain string, result bool) {
+	if len(chain) == 0 {
+		fmt.Println("The chain is empty!")
+		return chain, false
+	} else if chain[0] == letter {
+		if len(chain) > 1 {
+			chain = chain[1:]
+		} else {
+			chain = ""
+		}
+		return chain, true
+	} else {
+		fmt.Printf("The chain does not match '%v'!\n", string(letter))
+		return chain, false
+	}
+}
+
+func apply(rule string, chain string) (new_chain string, result bool) {
+	if len(chain) == 0 {
+		fmt.Println("Zero length chain, but rules remain - rule is:", rule)
+		return chain, false
+	}
+	fmt.Println(" options:", rule)
+	original_chain := chain
+	options := strings.Split(rule, " | ")
+	option_good := false
+	for _, option := range options {
+		option_good = true // assume it worked until it doesn't
+		sequence := strings.Split(option, " ")
+		fmt.Println("sequence: ", sequence)
+	inner:
+		for _, s := range sequence {
+			fmt.Println(" s: ", s)
+			if s == rule_a {
+				fmt.Println("a")
+				chain, result = chop(chain, 'a')
+				if !result {
+					fmt.Println(" -> Restore chain to:", original_chain)
+					option_good = false
+					break inner
+				}
+				fmt.Println(" chain is now:", chain)
+			} else if s == rule_b {
+				fmt.Println("b")
+				chain, result = chop(chain, 'b')
+				if !result {
+					fmt.Println(" -> Restore chain to:", original_chain)
+					option_good = false
+					break inner
+				}
+				fmt.Println(" chain is now:", chain)
+			} else {
+				chain, result = apply(rules[s], chain)
+				if result {
+					// do the next "s" in sequence, as we had a good result
+					continue inner
+				} else {
+					fmt.Printf("Sequence %v has failed\n", sequence)
+					option_good = false
+					break inner
+				}
+			}
+		}
+		fmt.Println("Reached the end of sequence:", sequence)
+		if option_good {
+			// don't want to do the next option because the first one worked
+			return chain, true
+		} else {
+			chain = original_chain
+		}
+	}
+	if !option_good {
+		// no good options found
+		fmt.Println("no good options found")
+		chain = original_chain
+		return chain, false
+	}
+	// we can get here if there were no option, so option_good never got set to false
+	fmt.Println("Reached the end of the rule:", rule)
+	return chain, true
+}
+
 // Run the program with the argument "2" to run part 2, or anything else for part 1.
 func main() {
 	get_rules()
+	fmt.Println("rule_a is:", rule_a)
+	fmt.Println("rule_b is:", rule_b)
 	if len(os.Args) > 1 && os.Args[1] == "2" {
 		fmt.Println("part 2")
 		// some rules to change for part 2
@@ -175,6 +270,26 @@ func main() {
 		rules["11"] = "42 31 | 42 11 31"
 	}
 	get_input()
-	answer := run()
-	fmt.Printf("The answer is %v\n", answer)
+	num_valid := 0
+	valid := []string{}
+
+	// single case testing:
+	input = []string{"aaaaabbaabaaaaababaa"} // invalid in part 1, valid in part 2
+	//input = []string{"bbabbbbaabaabba"}      // valid in both parts
+
+	for _, chain := range input {
+		fmt.Println("Chain:", chain)
+		final_chain, result := apply(rules["0"], chain)
+		if len(final_chain) == 0 {
+			fmt.Println("valid!")
+			if result {
+				num_valid++
+				valid = append(valid, chain)
+			}
+		} else {
+			fmt.Println("invalid - data remains:", final_chain)
+		}
+	}
+	fmt.Println("Number valid:", num_valid)
+	fmt.Println(valid)
 }
